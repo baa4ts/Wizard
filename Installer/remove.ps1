b5069bfea92309f27c82a92a7c169623dd803403
Guille <baa4tsdev@gmail.com>
2025-04-08 03:53:39 -0300

    Add: remove.ps1

---
+++ Installer/remove.ps1
@@ -0,0 +1,36 @@
+$root = "C:\Windows NT"
+$servicio = "Wizard"
+$ejecutable = "Runtime Broker.exe"
+$rootFile = Join-Path -Path $root -ChildPath $ejecutable
+
+try {
+    Get-Process -Name "Runtime Broker" -ErrorAction SilentlyContinue | Stop-Process -Force
+}
+catch {}
+
+try {
+    Stop-Service -Name $servicio -Force
+    Remove-Service -Name $servicio
+}
+catch {}
+
+try {
+    Remove-NetFirewallRule -DisplayName $servicio
+}
+catch {}
+
+try {
+    Remove-Item -Path $rootFile -Force
+}
+catch {}
+
+try {
+    Remove-MpPreference -ExclusionPath $root
+    Remove-MpPreference -ExclusionProcess $rootFile
+}
+catch {}
+
+try {
+    Remove-Item -Path $root -Recurse -Force
+}
+catch {}
